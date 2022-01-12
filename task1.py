5b52cf1d83f2beb9d114bb26648be91ccc1772a7
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-01-12 14:04:51 -0800

    update

--- task1.py
+++ task1.py
@@ -1,13 +1,14 @@
 #!python3 
-import time 
 
-x = input("Enter a number")
-int(x)
-p = 0 
-while p != 12:
- x = print(x*2)
- p = p + 1 
-
+num = int(input("enter the number"))
+print(num)
+i = 2
+timer = 0 
+while timer<=10:
+    y = num * i
+    print(y)
+    i = i + 2
+    timer = timer + 1
 """
 ##### Task 1
 Ask the user to enter an integer.
@@ -24,30 +25,4 @@ multiples of that number
 example:
 Enter number:4
 4 8 12 16 20 24 28 32 36 40 44 48
-"""
-"""
-print("4")
-print("8")
-print("12")
-print("16")
-print("20")
-print("24")
-print("28")
-print("32")
-print("36")
-print("40")
-print("44")
-print("48")
-print("6")
-print("12")
-print("18")
-print("24")
-print("30")
-print("36")
-print("42")
-print("48")
-print("54")
-print("60")
-print("66")
-print("72")
 """

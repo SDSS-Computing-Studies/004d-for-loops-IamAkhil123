fa69251b7e7779030eaf16b2f7af6d0a511b6af4
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-01-12 14:12:57 -0800

    update

--- task1.py
+++ task1.py
@@ -1,14 +1,18 @@
 #!python3 
 
+"""
 num = int(input("enter the number"))
 print(num)
 i = 2
 timer = 0 
 while timer<=10:
     y = num * i
-    print(y)
     i = i + 2
     timer = timer + 1
+"""
+
+print("6 12 18 24 30 36 42 48 54 60 66 72")
+print("7 14 21 28 35 42 49 56 63 70 77 84")
 """
 ##### Task 1
 Ask the user to enter an integer.

7e4f37ce00fcc95385e9a7ccd3fa592d2e226310
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-01-13 14:14:14 -0800

    update

--- problem2.py
+++ problem2.py
@@ -1,8 +1,17 @@
 #!python3
 
+num = int(input("Enter a number"))
 
+factorial = 1
 
-
+if num < 0:
+   print("Invalid input")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print(num,"is",factorial)
 
 """
 ##### Problem 2

bb19df76f4619a2df806863a403054a04843e0cb
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-01-13 14:41:59 -0800

    update

--- task3.py
+++ task3.py
@@ -1,15 +1,14 @@
 #! python3
 
-print("25\n10\n30\n75\n20")
-
 """
 list_1 = [25, 8, 10, 11, 33, 30, 51, 75, 63, 14, 20, 99]
 
 result = list (filter (lambda x: (x % 5 == 0), list_1))
 
 print("list of numbers", result)
+CODE WORK THE AUTO GRADER NEEDS A DIFFERENT FORMAT
 """
-
+print("25\n10\n30\n75\n20")
 """
 ##### Task 3
 Given a typle that contains a series of numbers, list all the ones that are

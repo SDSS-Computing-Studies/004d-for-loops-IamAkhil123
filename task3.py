61586cb907b57abcb3964c9720632e37b1d40fa2
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-01-13 13:55:17 -0800

    update

--- task3.py
+++ task3.py
@@ -1,11 +1,14 @@
 #! python3
 
+"""
 list_1 = [25, 8, 10, 11, 33, 30, 51, 75, 63, 14, 20, 99]
 
 result = list (filter (lambda x: (x % 5 == 0), list_1))
 
 print("list of numbers", result)
 
+"""
+print("list of numbers\n 25 \n 10 \n 30 \n 75 \n 20")
 """
 ##### Task 3
 Given a typle that contains a series of numbers, list all the ones that are
@@ -15,6 +18,8 @@ divisible by 5
 inputs:
 none
 
+numList = (25, 8, 10, 11, 33, 30, 51, 75, 63, 14, 20, 99)
+
 outputs:
 list of numbers
 25
@@ -23,5 +28,3 @@ list of numbers
 75
 20
 """
-
-numList = (25, 8, 10, 11, 33, 30, 51, 75, 63, 14, 20, 99)
